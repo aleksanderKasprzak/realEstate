ec9f9e2c17654903b66b64a96971618973d2762e
Aleksander Kasprzak <aleksander.kasprzak@spaceit.tech>
2024-03-20 16:23:37 +0100

    add error handling, provider to store

--- api/index.js
+++ api/index.js
@@ -27,3 +27,13 @@ app.listen(3000, () => {
 app.use('/api/user', UserRouter);
 app.use('/api/listing', ListingRouter);
 app.use('/api/auth', AuthRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
